08870f52ae91a5b019c164637d1d6991728f4da9
Bachtiar <bahtiarvivo123@gmail.com>
2025-05-29 21:06:17 +0700

    inisialisasi keknya

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import tkinter as tk
+from tkinter import ttk, messagebox
+from PIL import Image, ImageTk
 
-a = 10
+class our_app:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Aplikasi TKinter Seadanya")
+        self.root.geometry("1000x1000")
+        self.root.resizable(True,True)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = our_app(root)
+    root.mainloop()
